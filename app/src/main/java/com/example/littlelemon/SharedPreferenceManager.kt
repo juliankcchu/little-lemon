c44e774e4d08015dd74177821595ba21f2cf741d
Julian K C CHU <julian.kc.chu@gmail.com>
2024-08-18 01:52:18 +0800

    Update to process registration.

---
+++ app/src/main/java/com/example/littlelemon/SharedPreferenceManager.kt
@@ -0,0 +1,22 @@
+package com.example.littlelemon
+
+import android.content.Context
+import android.content.SharedPreferences
+
+// https://chintanjoshi1.medium.com/using-sharedpreferences-in-android-jetpack-compose-f8e970ffbf06
+class SharedPreferenceManager (context: Context) {
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+    fun saveData(key: String, value: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun getData(key: String, defaultValue: String): String {
+        return sharedPreferences.getString(key, defaultValue) ?: defaultValue
+    }
+
+}
